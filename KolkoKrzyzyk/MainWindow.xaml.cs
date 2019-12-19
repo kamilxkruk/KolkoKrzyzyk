6bb2ab3f18178d58cb460b44f74515fd38e725a9
kamilxkruk@gmail.com <kamilxkruk@gmail.com>
2019-12-19 18:31:58 +0100

    First tic tac toe class

--- KolkoKrzyzyk/MainWindow.xaml.cs
+++ KolkoKrzyzyk/MainWindow.xaml.cs
@@ -20,9 +20,67 @@ namespace KolkoKrzyzyk
     /// </summary>
     public partial class MainWindow : Window
     {
+        enum ButtonState
+        {
+            Circle,
+            Cross,
+            Empty
+        }
+
+        private ButtonState[] buttonStates;
+        private bool gameEnd = false;
+        private bool ifPlayerOne = true;
+
         public MainWindow()
         {
             InitializeComponent();
+            buttonStates = new ButtonState[9];
+            NewGame();
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (gameEnd)
+            {
+                NewGame();
+            }
+
+            Button currentButton = (Button)sender;
+
+            var currentRow = Grid.GetRow(currentButton);
+            var currentColumn = Grid.GetColumn(currentButton);
+
+            var index = currentColumn + currentRow * 3;
+
+            MessageBox.Show(index.ToString());
+
+            if(currentButton.Content != string.Empty)
+            {
+                return;
+            }
+
+            if (ifPlayerOne)
+            {
+                currentButton.Content = "X";
+                buttonStates[index] = ButtonState.Cross;
+            }
+            else
+            {
+                currentButton.Content = "O";
+                buttonStates[index] = ButtonState.Circle;
+            }
+
+            ifPlayerOne = !ifPlayerOne;
+        }
+
+        private void NewGame()
+        {
+            Container.Children.Cast<Button>().ToList().ForEach(button => button.Content = string.Empty);
+
+            for (int i = 0; i < 9; i++)
+            {
+                buttonStates[i] = ButtonState.Empty;
+            }
         }
     }
 }
